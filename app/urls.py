210ba1ad423700fa97096537ab4ec2cd2b76caf3
Yasmim Martins <yasmim.martins.profissional@gmail.com>
2025-05-24 12:41:48 -0300

    version 7.0

--- app/urls.py
+++ app/urls.py
@@ -11,23 +11,30 @@ urlpatterns = [
     path('criarHistorico/', views.CreateHistorico , name='Create Historico' ) ,
     path('atualizarHistorico/', views.UpdateHistorico, name = 'Update Historico') , 
     path('deleteHistorico/', views.DeleteHistorico, name='Delete Historico'),
+    #get historico com filtros
+    path('visualizarHistorico/<int:pk>', views.visualizarHistoricoPeloID, name= 'Visualizer Historico pelo ID'),
+    path('visualizarHistorico/<data : MM-DD-YYYY>', views.VisualizarHistoricoPorDia , name= 'Visualizar o Historico pela data'),
 
     #Urls relacionadas aos Sensores
     path('visualizarSensor/',views.VisualizarSensores, name='Visualizar Sensores' ) , 
     path('criarSensor/', views.CreateSensor , name='Create Sensor' ) ,
     path('atualizarSensor/', views.UpdateSensores, name = 'Update Sensores') , 
     path('deleteSensor/', views.DeleteSensores, name='Delete Sensores'),
+    #get sensores com filtros
+    path('visualizarSensor/<int:pk>',views.visualizarSensoresPeloID, name = 'visualizar Sensores por ID'), 
+    path('visuslizarSensores/<str:tipo>', views.visualizarSensoresPeloTipo , name = 'visuzalizar sensores pelo tipo'),
 
     #Urls relacionadas aos Ambientes
     path('visualizarAmbiente/',views.VisualizarAmbiente, name='Visualizar Ambientes' ) , 
     path('criarAmbiente/', views.CreateAmbiente , name='Create Ambiente' ) ,
-    path('atualizarAmbiente/', views.UpdateSensores, name = 'Update Sensores') , 
-    path('deleteSensor/', views.DeleteSensores, name='Delete Sensores'),
-
+    path('atualizarAmbiente/', views.UpdateAmbiente, name = 'Update Ambiente') , 
+    path('deleteAmbiente/', views.DeleteAmbientes, name='Delete ambientes'),
+    #get ambientes com filtros
+    path('visualizarAmbiente/<int:sig>', views.visualizarAmbientesPeloSig , name = 'Vizualizar ambiente pelo Sig'),
 
     #Extrair pdfs
     path('extrairSensores/', views.ExtrairXLSXSensores, name='Extrair Sensores'),
-    path('extrairAmbientes', views.ExtrairXLSXAmbientes, name='Extrair Ambientes')
+    path('extrairAmbientes', views.ExtrairXLSXAmbientes, name='Extrair Ambientes') ,
 
 
 ]
